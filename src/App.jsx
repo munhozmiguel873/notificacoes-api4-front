03dae83bedf62ddfa580809cfac9dbb003900daa
Pedro Augusto Fernandes Francelino <pedro.francelino@aluno.senai.br>
2026-09-15 09:20:15 -0300

    Alterando o estilo do app

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
+    <div className="max-w-2xl mx-auto p-4 bg-fundo-principal">
       <h1 className="text-2xl font-bold mb-4">Central de Notificações</h1>
 
       <NovaNotificacaoForm onAdicionar={adicionarNotificacao} />
